2ec32713dd7dab6d66b51f5fb78e5d0ad767c877
VotreNom <gribaaines5@gmail.com>
2025-09-24 15:33:01 +0200

    dé commit

--- app/src/main/java/com/example/premiereapplication/MainActivity.kt
+++ app/src/main/java/com/example/premiereapplication/MainActivity.kt
@@ -1,31 +1,25 @@
 package com.example.premiereapplication
 
-import ads_mobile_sdk.h6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.premiereapplication.ui.theme.PremiereApplicationTheme
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
-import coil.request.ImageRequest
+import com.example.premiereapplication.ui.theme.PremiereApplicationTheme
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -38,15 +32,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
                         TopAppBar(
-                            title = { Text("Événement", color = Color.Black) },
+                            title = { Text("Inscription", color = Color.Black, fontWeight = FontWeight.Bold) },
                             navigationIcon = {
-                                IconButton(onClick = { /* Action pour le menu burger */ }) {
+                                IconButton(onClick = { }) {
                                     Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu", tint = Color.Black)
                                 }
                             },
                             actions = {
-                                IconButton(onClick = { /* Action pour le cœur */ }) {
-                                    Icon(imageVector = Icons.Default.Favorite, contentDescription = "Favori", tint = Color.Black)
+                                IconButton(onClick = { }) {
+                                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Authentification", tint = Color.Black)
                                 }
                             },
                             colors = TopAppBarDefaults.topAppBarColors(
@@ -64,58 +58,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun EventDetails(modifier: Modifier = Modifier) {
+
+    val images = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
+
+    var currentImage by remember { mutableStateOf(images.random()) }
+
     Column(modifier = modifier.padding(16.dp)) {
-        /*Image(
-            painter = painterResource(id = R.drawable.capture),
-            contentDescription = "Affiche de l'événement",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            contentScale = ContentScale.Crop
-        )*/
-        val imageUrl = "https://wpadmin.uk2.net/app/uploads/sites/4/2016/06/shutterstock_441169243-770x514.jpg"
 
-        AsyncImage(
-            model = imageUrl,
-
+        Text(
+            text = "Générez votre numéro de passage !",
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Image(
+            painter = painterResource(id = currentImage),
             contentDescription = "Affiche de l'événement",
             modifier = modifier
                 .fillMaxWidth()
                 .height(300.dp)
                 .padding(bottom = 16.dp),
-        contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop
         )
 
-        Text(
-            text = "Où : Ecole ingénieur ISIS",
-            fontWeight = FontWeight.Bold,
-            color = Color.Gray
-        )
-        Text(
-            text = "Quand : 24 octobre",
-            fontWeight = FontWeight.Bold,
-            color = Color.Gray,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
 
-        // Boutons d'action
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Button(
-                onClick = { /* Action d'inscription */ },
+                onClick = {
+
+                    currentImage = images.random()
+                },
                 modifier = Modifier.weight(1f)
             ) {
-                Text(text = "Inscription")
+                Text(text = "Roll")
             }
             Button(
-                onClick = { /* Action "Pas intéressé" */ },
+                onClick = {},
                 modifier = Modifier.weight(1f),
                 colors = ButtonDefaults.buttonColors(containerColor = Color.LightGray)
             ) {
-                Text(text = "Pas intéressé")
+                Text(text = "Retour")
             }
         }
     }
